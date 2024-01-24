760a622a1bd8cf236a3a4e9632079f8edc44f571
JustinDR96 <justinderoover@gmail.com>
2024-01-24 10:51:05 +0100

    add proxy

--- api/proxy.js
+++ api/proxy.js
@@ -2,10 +2,18 @@
 import axios from "axios";
 
 export default async (req, res) => {
-  const url = `https://api.igdb.com${req.url.replace(/^\/api/, "/v4")}`;
+  const url = `https://api.igdb.com/v4${req.url.replace(/^\/api\/proxy/, "")}`;
+  const clientId = import.meta.env.VITE_CLIENT_ID;
+  const accessToken = import.meta.env.ACCESS_TOKEN;
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.post(url, req.body, {
+      headers: {
+        Accept: "application/json",
+        "Client-ID": clientId,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ error: "Error fetching data" });
