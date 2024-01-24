309306c533e39a7891a781ae6a16e0bfa548a6fb
De Roover Justin <justinderoover@gmail.com>
2024-01-24 19:55:12 +0100

    deploy

---
+++ src/api/server.js
@@ -0,0 +1,31 @@
+import express, { json, urlencoded } from "express";
+import axios from "axios";
+import cors from "cors";
+import authMiddleware from "./middleware/auth.cjs";
+
+// import route
+import gamesRouter from "./routes/fetchGames.cjs";
+import popularGamesRouter from "./routes/popularGames.cjs";
+import preorderGamesRouter from "./routes/preorderGames.cjs";
+import trendingGamesRouter from "./routes/trendingGames.cjs";
+import detailsGamesRouter from "./routes/detailsGames.cjs";
+import searchGamesRouter from "./routes/searchGames.cjs";
+
+const app = express();
+
+app.use(authMiddleware);
+app.use(cors());
+app.use(json()); // Utilisez express.json() comme middleware directement
+app.use(urlencoded({ extended: true }));
+// routes
+app.use("/", gamesRouter);
+app.use(popularGamesRouter);
+app.use(preorderGamesRouter);
+app.use(trendingGamesRouter);
+app.use("/games", detailsGamesRouter);
+app.use("/search", searchGamesRouter);
+
+export default (req, res) => {
+  const { method, url } = req;
+  return app.handle(req, res);
+};
