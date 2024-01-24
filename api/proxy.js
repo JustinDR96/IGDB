7a4bb0c5040ec4449e931ea481e2849b115c1560
JustinDR96 <justinderoover@gmail.com>
2024-01-24 12:00:26 +0100

    f

--- api/proxy.js
+++ api/proxy.js
@@ -1,15 +1,16 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
+import useAuth from "../src/hook/auth";
 
 export default (req, res) => {
   let target = "https://api.igdb.com/v4/games";
-
+  const accessToken = useAuth();
   // Create a proxy middleware
   const proxy = createProxyMiddleware({
     target,
     changeOrigin: true,
     onProxyReq: (proxyReq) => {
       proxyReq.setHeader("Client-ID", process.env.VITE_CLIENT_ID);
-      proxyReq.setHeader("Authorization", `Bearer ${process.env.ACCESS_TOKEN}`);
+      proxyReq.setHeader("Authorization", `Bearer ${accessToken}`);
     },
   });
 
