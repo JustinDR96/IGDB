aed25396ba4c85f913856412df1f0fdb367895a3
JustinDR96 <justinderoover@gmail.com>
2024-01-24 12:06:20 +0100

    f

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 import useAuth from "../src/hook/auth";
 
 export default (req, res) => {
-  let target = "https://api.igdb.com/v4/games";
+  let target = "https://api.igdb.com/v4";
   const accessToken = useAuth();
   // Create a proxy middleware
   console.log(accessToken);
